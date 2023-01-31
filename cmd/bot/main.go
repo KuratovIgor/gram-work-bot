fbd2ead632c26376a29272670f883346a8621da3
Igor Kuratov <kuratov.ia@dclouds.ru>
2023-02-01 00:48:03 +0700

    set project structure, create base bot

---
+++ cmd/bot/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/KuratovIgor/gram-work-bot/pkg/telegram"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+func main() {
+	// TODO: Добавить перемунную из окружения
+	bot, err := tgbotapi.NewBotAPI("")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.NewBot(bot)
+
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ pkg/telegram/handlers.go
@@ -0,0 +1,30 @@
+package telegram
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+const commandStart = "start"
+
+func (b *Bot) handleCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Я не знаю такой команды :(")
+
+	switch message.Command() {
+	case commandStart:
+		msg.Text = "START"
+		_, error := b.bot.Send(msg)
+		return error
+	default:
+		_, error := b.bot.Send(msg)
+		return error
+	}
+}
+
+func (b *Bot) handleMessage(message *tgbotapi.Message) {
+	log.Printf("[%s] %s", message.From.UserName, message.Text)
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
+
+	b.bot.Send(msg)
+}
