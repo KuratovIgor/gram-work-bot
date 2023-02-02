2b5a9ba060b79ef7cb6eb41397a7d21e3054f705
Igor Kuratov <kuratov.ia@dclouds.ru>
2023-02-03 00:49:16 +0700

    remove env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/spf13/viper"
-	"os"
 )
 
 type Config struct {
@@ -65,8 +64,6 @@ func unmarshal(cfg *Config) error {
 }
 
 func parseEnv(cfg *Config) error {
-	os.Setenv("TELEGRAM_TOKEN", "5625272170:AAGQVFOEIh_aoRMUfB3vBXx6QrDBM5sLYro")
-
 	if err := viper.BindEnv("telegram_token"); err != nil {
 		return err
 	}
